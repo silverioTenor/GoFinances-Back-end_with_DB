1873f676a1179a04df17a245d5e95de97ca270ff
silverioTenor <silverio_tenor@outook.com>
2021-01-08 14:16:26 -0300

    Criação das primeiras migrations

---
+++ src/database/migrations/1610125735561-AlterCategoryColumn.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
+
+export default class AlterCategoryColumn1610125735561 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        name: 'transactions_category_id_fkey',
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'transactions_category_id_fkey');
+  }
+}
