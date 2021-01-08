03eb9ffda76d690f151510be20960ee9cc70d63a
silverioTenor <silverio_tenor@outook.com>
2021-01-08 12:34:14 -0300

    Ajustes

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+app.use((err: Error, request: Request, response: Response, _next: NextFunction) => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
       status: 'error',
