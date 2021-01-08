bac6e0c06ad49d3c8cae2215f9c211941011dd85
silverioTenor <silverio_tenor@outook.com>
2021-01-08 14:25:31 -0300

    Renomeação dos serviços.

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import CreateTransactionService from '../services/CreateTransaction.service';
+// import DeleteTransactionService from '../services/DeleteTransaction.service';
+// import ImportTransactionsService from '../services/ImportTransactions.service';
 
 const transactionsRouter = Router();
 
@@ -13,6 +13,12 @@ transactionsRouter.get('/', async (request, response) => {
 
 transactionsRouter.post('/', async (request, response) => {
   const { title, value, type, category } = request.body;
+
+  const createTransaction = new CreateTransactionService();
+
+  const transaction = await createTransaction.execute({ title, value, type, category });
+
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
